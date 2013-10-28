28db2a8357facf0c51c20063437eeda0fa0cde1e
morganbohn <morgan.bohn@gmail.com>
2013-10-28 15:17:20 +0100

    load mir list

--- src/pyromarc-template/template.py
+++ src/pyromarc-template/template.py
@@ -17,40 +17,57 @@ class Template(object):
     def __init__(self, spec):
         """ load the yaml file into dict """ 
         self.spec = yaml.safe_load(spec)
-        self.data = {}
+        self.datas = {}
         self.template = {}
 
-    def _load_mir_str(self,d):
+    def _load_mir_str(self, data):
         """ load mir str """
-        return {self.spec[d[0]] : d[1],}
+        return {self.spec[data[0]] : data[1],}
         
-    def _load_mir_dict(self, d):
+    def _load_mir_dict(self, data):
         """ load mir dict """
-        return {self.spec[d[0]][i[0]]:i[1] for i in d[1]}
+        return {self.spec[data[0]][elem[0]]:elem[1] for elem in data[1]}
 
-    def _load_mir_list(self, d):
+    def _load_mir_list(self, data):
         """ load mir list """
-        log.logger.info("TODO load_mir_list")
-        return {}
+        subdict = {}
+        for elem in data[1]:
+            key = self.spec[data[0]][1][elem[0]]
+            value = elem[1]
 
-    def load_mir_data(self, data):
+            if key in subdict:
+                if isinstance(subdict[key], list):
+                    subdict[key].append(value)
+                else:
+                    subdict[key] = [subdict[key], value ]
+            else:
+                subdict[key] = value 
+
+        return [subdict,]
+
+    def load_mir_data(self, datas):
         """ load mir data into template"""
-        self.data = yaml.safe_load(data)
-        for d in self.data:
-            spec_value = self.spec[d[0]] 
+        self.datas = yaml.safe_load(datas)
+        for data in self.datas:
+            spec_value = self.spec[data[0]] 
             if isinstance(spec_value, str):
-                self.template.update(self._load_mir_str(d))
+                self.template.update(self._load_mir_str(data))
             elif isinstance(spec_value, dict):
-                self.template.update(self._load_mir_dict(d))
+                self.template.update(self._load_mir_dict(data))
             elif isinstance(spec_value, list):
-                self.template.update(self._load_mir_list(d))
+                parent_key = spec_value[0]
+                if parent_key in self.template:
+                    concat_list = self._load_mir_list(data) + self.template[parent_key]
+                    self.template[parent_key] = concat_list
+                else:
+                    self.template.update({parent_key: self._load_mir_list(data),})
 
 
 if __name__=="__main__":
     log.logger.info("start pyromarc template")
     
     url = "/home/morgan/dev/pyromarc-template/src/pyromarc-template/spectest.yml"
-    data = """
+    datas = """
     - [001, PPNxxxx ]
     - [200, [ [a, Doe], [b, john], [b, elias], [b, frederik] ]]
     - [200, [ [a, Doe], [b, jane]                            ]]
@@ -59,8 +76,8 @@ if __name__=="__main__":
     
     with open(url) as f:
         template = Template(f)
-        template.load_mir_data(data)
+        template.load_mir_data(datas)
 
     log.logger.info("Spec: %s" % (template.spec,))
-    log.logger.info("Data: %s" % (template.data,))
+    log.logger.info("Data: %s" % (template.datas,))
     log.logger.info("Template: %s" % (template.template,))
