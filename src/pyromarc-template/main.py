fa1fd379212da46792e4294b4d034cb75d63bda1
morganbohn <morgan.bohn@gmail.com>
2013-10-28 17:44:18 +0100

    modifiy main

--- src/pyromarc-template/main.py
+++ src/pyromarc-template/main.py
@@ -6,18 +6,31 @@ if __name__=="__main__":
     log.logger.info("start pyromarc template\n----------")
     
     url = "./spectest.yml"
-    datas = """
+    datas_mir = """
     - [001, PPNxxxx ]
     - [200, [ [a, Doe], [b, john], [b, elias], [b, frederik] ]]
     - [200, [ [a, Doe], [b, jane]                            ]]
     - [300, [ [a, "i can haz title"], [b, "also subs"]       ]]
     """
-    
+
+    template_dict = """
+    authors:
+        - { name: Doe, firstname: [john, elias, frederik] }
+        - { name: Doe, firstname: jane }
+    title: "i can haz title"
+    subtitle: "also subs"
+    id: PPNxxxx
+    """
+
     with open(url) as f:
         template = Template(f)
-        template.load_mir_data(datas)
-
-    log.logger.info("Spec: %s\n----------" % (template.spec,))
-    log.logger.info("Data: %s\n----------" % (template.datas,))
-    log.logger.info("Template: %s\n----------" % (template.template,))
+        log.logger.info("Spec: %s\n----------\n" % (template.spec,))
+        template.load_mir_data(datas_mir)
+        log.logger.info("FROM DATA TO TEMPLATE\n----------")
+        log.logger.info("Data: %s\n----------" % (template.datas,))
+        log.logger.info("Template: %s\n\n" % (template.template,))
+        template.load_dict_template(template_dict)
+        log.logger.info("FROM TEMPLATE TO DATA\n----------")
+        log.logger.info("Data: %s\n----------" % (template.datas,))
+        log.logger.info("Template: %s\n\n" % (template.template,))
 
